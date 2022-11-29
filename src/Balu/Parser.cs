6060e0131f5132aa0b4f3a6cbf84316002b86013
Rene Vogt <rvogt@aki-gmbh.com>
2022-11-29 16:32:44 +0100

    implement first parser steps

--- src/Balu/Parser.cs
+++ src/Balu/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using Balu.Expressions;
 using System;
+using System.Linq;
 
 namespace Balu;
 
@@ -8,7 +9,9 @@ namespace Balu;
 /// </summary>
 sealed class Parser : IParser
 {
+    static readonly NumberExpressionSyntax dummyNumber = new NumberExpressionSyntax(SyntaxToken.Number(0, 0, string.Empty));
     readonly ILexer lexer;
+
 
     /// <summary>
     /// Creates a new <see cref="Parser"/> for the given <paramref name="input"/> of Balu code.
@@ -25,6 +28,25 @@ sealed class Parser : IParser
     /// <inheritdoc/>
     public ExpressionSyntax Parse()
     {
+        var tokens = lexer.GetTokens()
+                          .Where(token => token.Kind != SyntaxKind.BadToken && token.Kind != SyntaxKind.WhiteSpaceToken &&
+                                          token.Kind != SyntaxKind.EndOfFileToken)
+                          .ToArray();
+
+        if (tokens.Length == 0) return dummyNumber;
         
+        int position = 0;
+        var left = ParseNumberExpression();
+        while (position < tokens.Length)
+        {
+            var operatorToken = tokens[position++];
+            var right = ParseNumberExpression();
+            left = ExpressionSyntax.Binary(left, operatorToken, right);
+        }
+
+        return left;
+
+        ExpressionSyntax ParseNumberExpression() =>
+            position < tokens.Length && tokens[position].Kind == SyntaxKind.NumberToken ? new NumberExpressionSyntax(tokens[position++]) : dummyNumber;
     }
 }
