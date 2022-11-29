d7ea680ccffad163da2196f871ef016991f015a8
Rene Vogt <rvogt@aki-gmbh.com>
2022-11-29 14:11:43 +0100

    setup lexer infrastructure

---
+++ src/Balu/Lexer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balu;
+
+/// <summary>
+/// A lexer for the Balu language.
+/// </summary>
+sealed class Lexer : ILexer
+{
+    string input;
+    int position;
+
+    /// <summary>
+    /// Creates a new <see cref="Lexer"/> instance.
+    /// </summary>
+    /// <param name="input">The input Balu code.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="input"/> is <c>null</c>.</exception>
+    public Lexer(string input)
+    {
+        this.input = input ?? throw new ArgumentNullException(nameof(input));
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<SyntaxToken> GetTokens()
+    {
+        while (position < input.Length)
+        {
+            position++;
+        }
+
+        yield return new SyntaxToken(SyntaxKind.EndOfFileToken, position);
+    }
+}
