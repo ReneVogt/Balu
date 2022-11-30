8afeb4bd4df5778e5e23e756c92a2b8461ee5428
Rene Vogt <rvogt@aki-gmbh.com>
2022-11-30 22:15:00 +0100

    introducing binding

---
+++ src/Balu/Binding/BoundBinaryExpression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Balu.Binding;
+
+sealed class BoundBinaryExpression : BoundExpression
+{
+    public override BoundNodeKind Kind => BoundNodeKind.BinaryExpression;
+    public override Type Type => Left.Type;
+
+    public BoundExpression Left { get; }
+    public BoundBinaryOperatorKind OperatorKind { get; }
+    public BoundExpression Right { get; }
+    
+    public BoundBinaryExpression(BoundExpression left, BoundBinaryOperatorKind operatorKind, BoundExpression right)
+    {
+        Left = left;
+        OperatorKind = operatorKind;
+        Right = right;
+    }
+}
---
+++ src/Balu/Binding/BoundUnaryExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Balu.Binding;
+
+sealed class BoundUnaryExpression : BoundExpression
+{
+    public override BoundNodeKind Kind => BoundNodeKind.UnaryExpression;
+    public override Type Type => Operand.Type;
+
+    public BoundUnaryOperatorKind OperatorKind { get; }
+    public BoundExpression Operand { get; }
+
+    public BoundUnaryExpression(BoundUnaryOperatorKind operatorKind, BoundExpression operand) => (OperatorKind, Operand) = (operatorKind, operand);
+}
