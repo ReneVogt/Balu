d7bfd8a21f638759d6fbb9de5dc098e981a1cef5
Rene Vogt <rvogt@aki-gmbh.com>
2022-12-02 12:56:09 +0100

    intorduce compilation, refactor evaluation

---
+++ src/Balu/Evaluation/EvaluationResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Balu.Evaluation;
+
+/// <summary>
+/// Represents the result of a Balu code evaluation.
+/// </summary>
+public sealed class EvaluationResult
+{
+    /// <summary>
+    /// The errors that occured during lexing, parsing or binding.
+    /// </summary>
+    public IReadOnlyList<string> Diagnostics { get; }
+    /// <summary>
+    /// The resulting value of the evalution. This is <c>null</c> if there are any <see cref="Diagnostics"/>.
+    /// </summary>
+    public object? Value { get; }
+
+    internal EvaluationResult(IEnumerable<string> diagnostics, object? value) => (Diagnostics, Value) = (diagnostics.ToArray(), value);
+}
