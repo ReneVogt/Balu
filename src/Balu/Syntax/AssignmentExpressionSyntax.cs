3845b1290df515e3fcf5ae2dfe1cb1a204c90aa9
Rene Vogt <rvogt@aki-gmbh.com>
2022-12-03 18:31:49 +0100

    add equals and identifier token, name and assignment expressions, prepare parsing, add missing expressions to visitor

---
+++ src/Balu/Syntax/AssignmentExpressionSyntax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Balu.Syntax;
+
+public sealed class AssignmentExpressionSyntax : ExpressionSyntax
+{
+    public SyntaxToken IdentifierrToken { get; }
+    public SyntaxToken EqualsToken { get; }
+
+    public ExpressionSyntax Expression { get; }
+
+    /// <inheritdoc/>
+    public override SyntaxKind Kind => SyntaxKind.AssignmentExpression;
+    /// <inheritdoc/>
+    public override IEnumerable<SyntaxNode> Children 
+    {
+        get
+        {
+            yield return IdentifierrToken;
+            yield return EqualsToken;
+            yield return Expression;
+        }
+    }
+
+    internal AssignmentExpressionSyntax(SyntaxToken identifierrToken, SyntaxToken equalsToken, ExpressionSyntax expression) =>
+        (IdentifierrToken, EqualsToken, Expression) = (identifierrToken, equalsToken, expression);
+
+    internal override SyntaxNode Accept(SyntaxVisitor visitor)
+    {
+        SyntaxToken identifierToken = (SyntaxToken)visitor.Visit(IdentifierrToken);
+        SyntaxToken equalsToken = (SyntaxToken)visitor.Visit(EqualsToken);
+        ExpressionSyntax expression = (ExpressionSyntax)visitor.Visit(Expression);
+        return identifierToken != IdentifierrToken || equalsToken != EqualsToken || expression != Expression ? Assignment(identifierToken, equalsToken, expression) : this;
+    }
+}
---
+++ src/Balu/Syntax/NameExpressionSyntax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Balu.Syntax;
+
+public sealed class NameExpressionSyntax : ExpressionSyntax
+{
+    public SyntaxToken IdentifierrToken { get; }
+
+    /// <inheritdoc/>
+    public override SyntaxKind Kind => SyntaxKind.NameExpression;
+    /// <inheritdoc/>
+    public override IEnumerable<SyntaxNode> Children 
+    {
+        get
+        {
+            yield return IdentifierrToken;
+        }
+    }
+
+    internal NameExpressionSyntax(SyntaxToken identifierrToken) =>
+        (IdentifierrToken) = (identifierrToken);
+
+    internal override SyntaxNode Accept(SyntaxVisitor visitor)
+    {
+        SyntaxToken identifierToken = (SyntaxToken)visitor.Visit(IdentifierrToken);
+        return identifierToken!= IdentifierrToken  ? Name(identifierToken) : this;
+    }
+}
