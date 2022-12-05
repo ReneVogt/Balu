4beb67c542e22d90c8252bd2f3f0604db995b789
Rene Vogt <rvogt@aki-gmbh.com>
2022-12-05 07:20:32 +0100

    added GetText method for SyntaxKind and unit-tests

--- src/Balu/Syntax/SyntaxFacts.cs
+++ src/Balu/Syntax/SyntaxFacts.cs
@@ -1,6 +1,15 @@
 ﻿namespace Balu.Syntax;
-static class SyntaxFacts
+
+/// <summary>
+/// Provides methods to evalute syntax facts.
+/// </summary>
+public static class SyntaxFacts
 {
+    /// <summary>
+    /// Determines the precedence of an unary operator.
+    /// </summary>
+    /// <param name="kind">The <see cref="SyntaxKind"/> of the unary operator.</param>
+    /// <returns>The precedence of the given operator.</returns>
     public static int UnaryOperatorPrecedence(this SyntaxKind kind) => kind switch
     {
         SyntaxKind.PlusToken or
@@ -8,6 +17,11 @@ static class SyntaxFacts
         SyntaxKind.BangToken => 100,
         _ => 0
     };
+    /// <summary>
+    /// Determines the precedence of a binary operator.
+    /// </summary>
+    /// <param name="kind">The <see cref="SyntaxKind"/> of the binary operator.</param>
+    /// <returns>The precedence of the given operator.</returns>
     public static int BinaryOperatorPrecedence(this SyntaxKind kind) => kind switch
     {
         SyntaxKind.SlashToken or
@@ -22,10 +36,39 @@ static class SyntaxFacts
         SyntaxKind.PipePipeToken => 1,
         _ => 0
     };
+    /// <summary>
+    /// Determines the <see cref="SyntaxKind"/> of a given keyword.
+    /// </summary>
+    /// <param name="literal">The string representing the keyword.</param>
+    /// <returns>The <see cref="SyntaxKind"/> of the given keyword, or <see cref="SyntaxKind.IdentifierToken"/> if it's not a keyword.</returns>
     public static SyntaxKind KeywordKind(this string literal) => literal switch
     {
         "true" => SyntaxKind.TrueKeyword,
         "false" => SyntaxKind.FalseKeyword,
         _ => SyntaxKind.IdentifierToken
+    };
+
+    /// <summary>
+    /// Gets the text a given <see cref="SyntaxKind"/> should have in Balu code.
+    /// </summary>
+    /// <param name="kind">The <see cref="SyntaxKind"/> to translate.</param>
+    /// <returns>A string representing the <paramref name="kind"/>.</returns>
+    public static string? GetText(this SyntaxKind kind) => kind switch
+    {
+        SyntaxKind.PlusToken => "+",
+        SyntaxKind.MinusToken => "-",
+        SyntaxKind.StarToken => "*",
+        SyntaxKind.SlashToken => "/",
+        SyntaxKind.OpenParenthesisToken => "(",
+        SyntaxKind.ClosedParenthesisToken => ")",
+        SyntaxKind.EqualsToken => "=",
+        SyntaxKind.BangToken => "!",
+        SyntaxKind.AmpersandAmpersandToken => "&&",
+        SyntaxKind.PipePipeToken => "||",
+        SyntaxKind.EqualsEqualsToken => "==",
+        SyntaxKind.BangEqualToken => "!=",
+        SyntaxKind.TrueKeyword => "true",
+        SyntaxKind.FalseKeyword => "false",
+        _ => null
     };
 }
