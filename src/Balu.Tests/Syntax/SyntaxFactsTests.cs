81efa4c88ece49a846e55ba2c4be57532b07655f
Rene Vogt <rvogt@aki-gmbh.com>
2022-12-05 07:20:32 +0100

    added GetText method for SyntaxKind and unit-tests

---
+++ src/Balu.Tests/Syntax/SyntaxFactsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Balu.Syntax;
+using Xunit;
+
+namespace Balu.Tests.Syntax;
+
+public class SyntaxFactsTests
+{
+    [Theory]
+    [MemberData(nameof(GetSyntaxKinds))]
+    public void SyntaxFacts_GetText_RoundTrips(SyntaxKind kind)
+    {
+        var text = kind.GetText();
+        if (text is null) return;
+        Assert.Equal(kind, Assert.Single(SyntaxTree.ParseTokens(text)).Kind);
+    }
+
+    public static IEnumerable<object[]> GetSyntaxKinds() =>
+        Enum.GetValues(typeof(SyntaxKind)).Cast<object>().Select(enumValue => new[] { enumValue });
+}
