0af5e7374f2f501654699759dab92fd982432e8d
Rene Vogt <rvogt@aki-gmbh.com>
2022-12-05 08:58:52 +0100

    implement tree asserter for unit tests

---
+++ src/Balu.Tests/SyntaxTreeAsserter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Balu.Syntax;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+
+namespace Balu.Tests
+{
+    sealed class SyntaxTreeAsserter : IDisposable
+    {
+        readonly IEnumerator<SyntaxNode> enumerator;
+        bool hasErrors;
+
+        public SyntaxTreeAsserter(SyntaxNode node) => enumerator = Flatten(node).GetEnumerator();
+        public void Dispose()
+        {
+            if (!hasErrors)
+                Assert.False(enumerator.MoveNext());
+            enumerator.Dispose();
+        }
+
+        static IEnumerable<SyntaxNode> Flatten(SyntaxNode node)
+        {
+            Stack<SyntaxNode> stack = new();
+            stack.Push(node);
+
+            while (stack.Count > 0)
+            {
+                var n = stack.Pop();
+                yield return n;
+                foreach (var child in n.Children.Reverse()) stack.Push(child);
+            }
+        }
+
+        bool Markfailed() => !(hasErrors = true);
+
+        public void AssertToken(SyntaxKind kind, string text)
+        {
+            try
+            {
+                Assert.True(enumerator.MoveNext());
+                Assert.Equal(kind, enumerator.Current.Kind);
+                var token = Assert.IsType<SyntaxToken>(enumerator.Current);
+                Assert.Equal(text, token.Text);
+            }
+            catch when (Markfailed())
+            {
+                throw;
+            }
+        }
+        public void AssertNode(SyntaxKind kind)
+        {
+            try
+            {
+                Assert.True(enumerator.MoveNext());
+                Assert.Equal(kind, enumerator.Current.Kind);
+                Assert.IsNotType<SyntaxToken>(enumerator.Current);
+            }
+            catch when (Markfailed())
+            {
+                throw;
+            }
+        }
+    }
+}
