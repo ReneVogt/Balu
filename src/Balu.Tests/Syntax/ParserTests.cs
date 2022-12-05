c1a3f12fdbf664b6f135e1f419e2b6540b0ca90e
Rene Vogt <rvogt@aki-gmbh.com>
2022-12-05 09:09:02 +0100

    test unary vs binary operator precedence

--- src/Balu.Tests/Syntax/ParserTests.cs
+++ src/Balu.Tests/Syntax/ParserTests.cs
@@ -51,48 +51,46 @@ public class ParserTests
     }
 
     [Theory]
-    [MemberData(nameof(ProvideBinaryOperatorPairs))]
-    public void Parser_UnaryExpression_HonorsPrecedences(SyntaxKind left, SyntaxKind right)
+    [MemberData(nameof(ProvideUnaryBinaryOperatorPairs))]
+    public void Parser_UnaryExpression_HonorsPrecedences(SyntaxKind unaryKind, SyntaxKind binaryKind)
     {
-        var leftPrecedence = left.BinaryOperatorPrecedence();
-        var leftText = left.GetText()!;
-        var rightPrecedence = right.BinaryOperatorPrecedence();
-        var rightText = right.GetText()!;
+        var unaryPrecedence = unaryKind.UnaryOperatorPrecedence();
+        var unaryText = unaryKind.GetText()!;
+        var binaryPrecedence = binaryKind.BinaryOperatorPrecedence();
+        var binaryText = binaryKind.GetText()!;
 
-        var text = $"a {leftText} b {rightText} c";
+        var text = $"{unaryText} a {binaryText} b";
         var tree = SyntaxTree.Parse(text);
 
-        if (leftPrecedence >= rightPrecedence)
+        if (unaryPrecedence >= binaryPrecedence)
         {
             using var e = new SyntaxTreeAsserter(tree.Root);
             e.AssertNode(SyntaxKind.BinaryExpression);
-            e.AssertNode(SyntaxKind.BinaryExpression);
+            e.AssertNode(SyntaxKind.UnaryExpression);
+            e.AssertToken(unaryKind, unaryText);
             e.AssertNode(SyntaxKind.NameExpression);
             e.AssertToken(SyntaxKind.IdentifierToken, "a");
-            e.AssertToken(left, leftText);
+            e.AssertToken(binaryKind, binaryText);
             e.AssertNode(SyntaxKind.NameExpression);
             e.AssertToken(SyntaxKind.IdentifierToken, "b");
-            e.AssertToken(right, rightText);
-            e.AssertNode(SyntaxKind.NameExpression);
-            e.AssertToken(SyntaxKind.IdentifierToken, "c");
         }
         else
         {
             using var e = new SyntaxTreeAsserter(tree.Root);
+            e.AssertNode(SyntaxKind.UnaryExpression);
+            e.AssertToken(unaryKind, unaryText);
             e.AssertNode(SyntaxKind.BinaryExpression);
             e.AssertNode(SyntaxKind.NameExpression);
             e.AssertToken(SyntaxKind.IdentifierToken, "a");
-            e.AssertToken(left, leftText);
-            e.AssertNode(SyntaxKind.BinaryExpression);
+            e.AssertToken(binaryKind, binaryText);
             e.AssertNode(SyntaxKind.NameExpression);
             e.AssertToken(SyntaxKind.IdentifierToken, "b");
-            e.AssertToken(right, rightText);
-            e.AssertNode(SyntaxKind.NameExpression);
-            e.AssertToken(SyntaxKind.IdentifierToken, "c");
-
         }
     }
     public static IEnumerable<object[]> ProvideBinaryOperatorPairs() => from left in SyntaxFacts.GetBinaryOperators()
                                                                         from right in SyntaxFacts.GetBinaryOperators()
                                                                         select new object[] { left, right };
+    public static IEnumerable<object[]> ProvideUnaryBinaryOperatorPairs() => from unary in SyntaxFacts.GetUnaryOperators()
+                                                                        from binary in SyntaxFacts.GetBinaryOperators()
+                                                                        select new object[] { unary, binary };
 }
