a0ba5633193c6a5e6ee61c9daa6b5175a58e58e9
Rene Vogt <rene.vogt@gmx.de>
2022-12-07 16:58:25 +0100

    added BoundScope

---
+++ src/Balu/Binding/BoundScope.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Balu.Binding;
+sealed class BoundScope
+{
+    readonly Dictionary<string, VariableSymbol> variables = new();
+
+    BoundScope? Parent { get; }
+    public BoundScope(BoundScope? parent) => Parent = parent;
+
+    public bool TryLookup(string name, out VariableSymbol variable) => variables.TryGetValue(name, out variable!) || (Parent?.TryLookup(name, out variable) ?? false);
+    public bool TryDeclare(VariableSymbol variable)
+    {
+        if (variables.ContainsKey(variable.Name)) return false;
+        variables.Add(variable.Name, variable);
+        return true;
+    }
+    public ImmutableArray<VariableSymbol> GetDeclaredVariables() => variables.Values.ToImmutableArray();
+}
