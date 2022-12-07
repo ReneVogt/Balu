9c13822902d25958b4b7560796ca8a1609e0bf7d
Rene Vogt <rene.vogt@gmx.de>
2022-12-07 17:05:18 +0100

    added BoundGlobalScope

---
+++ src/Balu/Binding/BoundGlobalScope.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Balu.Binding;
+sealed class BoundGlobalScope
+{
+    public BoundGlobalScope? Previous { get; }
+    public BoundExpression Expression { get; }
+    public ImmutableArray<VariableSymbol> Variables { get; }
+    public ImmutableArray<Diagnostic> Diagnostics { get; }
+
+    public BoundGlobalScope(BoundGlobalScope? previous, BoundExpression expression, IEnumerable<VariableSymbol> variables, IEnumerable<Diagnostic> diagnostics)
+    {
+        Previous = previous;
+        Expression = expression;
+        Variables = variables.ToImmutableArray();
+        Diagnostics = diagnostics.ToImmutableArray();
+    }
+
+}
