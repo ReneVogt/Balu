b94d8b1e1b42d270bc5b86e0d5d17cff715034dc
Rene Vogt <rene.vogt@gmx.de>
2022-12-10 16:56:41 +0100

    and another one

--- src/Balu.Tests/Evaluation/EvaluatorTests.cs
+++ src/Balu.Tests/Evaluation/EvaluatorTests.cs
@@ -166,6 +166,7 @@ public class EvaluatorTests
     [Theory]
     [InlineData("{ var x = 0 while (x < 12) x = x + 1 x }", 12)]
     [InlineData("{ var result = 1 var i = 0 while (i < 5) { i = i + 1 result = result * 2} result }", 32)]
+    [InlineData("{ var result = 0 var i = 0 while (i < 10) { i = i + 1 result = result + i} result }", 55)]
     public void Evaluate_WhileStatement_BasicallyWorks(string text, object? result) => text.AssertEvaluation(value: result);
     [Fact]
     public void Evaluate_WhileStatement_Reports_WrongConditionType()
