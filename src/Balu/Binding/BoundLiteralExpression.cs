f5e022d935605f5f6977aeb34e63d0d591d8dde5
Rene Vogt <rene.vogt@gmx.de>
2022-12-13 16:50:39 +0100

    initialize Type in constructor

--- src/Balu/Binding/BoundLiteralExpression.cs
+++ src/Balu/Binding/BoundLiteralExpression.cs
@@ -9,17 +9,21 @@ sealed class BoundLiteralExpression : BoundExpression
     public override BoundNodeKind Kind => BoundNodeKind.LiteralExpression;
     public override IEnumerable<BoundNode> Children { get; } = Array.Empty<BoundNode>();
 
-    public override TypeSymbol Type => Value switch
-    {
-        int => TypeSymbol.Integer,
-        bool => TypeSymbol.Boolean,
-        string => TypeSymbol.String,
-        _ => throw new BindingException($"Invalid literal value type '{Value.GetType().Name}'.")
-    };
+    public override TypeSymbol Type { get; }
 
     public object Value { get; }
 
-    public BoundLiteralExpression(object value) => Value = value;
+    public BoundLiteralExpression(object value)
+    {
+        Value = value;
+        Type = Value switch
+        {
+            int => TypeSymbol.Integer,
+            bool => TypeSymbol.Boolean,
+            string => TypeSymbol.String,
+            _ => throw new BindingException($"Invalid literal value type '{Value.GetType().Name}'.")
+        };
+    }
 
     internal override BoundNode Accept(BoundTreeVisitor visitor) => this;
 
