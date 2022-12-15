c8539392eb5d8a0f2d27b89ef79a67553dd08b2b
Rene Vogt <rene.vogt@gmx.de>
2022-12-15 12:55:29 +0100

    optimized memory usage

--- src/Balu/Binding/BoundScope.cs
+++ src/Balu/Binding/BoundScope.cs
@@ -1,33 +1,42 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Balu.Symbols;
 
 namespace Balu.Binding;
 sealed class BoundScope
 {
-    readonly Dictionary<string, VariableSymbol> variables = new();
-    readonly Dictionary<string, FunctionSymbol> functions = new();
+    Dictionary<string, VariableSymbol>? variables;
+    Dictionary<string, FunctionSymbol>? functions;
 
     public BoundScope? Parent { get; }
     public BoundScope(BoundScope? parent)
     {
         Parent = parent;
         if (Parent is null)
-            foreach (var function in BuiltInFunctions.GetBuiltInFunctions())
-                functions.Add(function.Name, function);
+            functions = BuiltInFunctions.GetBuiltInFunctions().ToDictionary(function => function.Name, function => function);
     }
 
-    public bool TryLookupVariable(string name, out VariableSymbol variable) => variables.TryGetValue(name, out variable!) || (Parent?.TryLookupVariable(name, out variable) ?? false);
-    public bool TryDeclareVariable(VariableSymbol variable) => TryDeclare(variable, variables);
-    public ImmutableArray<VariableSymbol> GetDeclaredVariables() => variables.Values.ToImmutableArray();
-
-    public bool TryLookupFunction(string name, out FunctionSymbol function) => functions.TryGetValue(name, out function!) || (Parent?.TryLookupFunction(name, out function) ?? false);
-    public bool TryDeclareFunction(FunctionSymbol function) => TryDeclare(function, functions);
-    public ImmutableArray<FunctionSymbol> GetDeclaredFunctions() => functions.Values.ToImmutableArray();
-
-
-    static bool TryDeclare<T>(T symbol, Dictionary<string, T> store) where T : Symbol
+    public bool TryLookupVariable(string name, out VariableSymbol variable)
     {
+        variable = null!;
+        return (variables?.TryGetValue(name, out variable!) ?? false) || (Parent?.TryLookupVariable(name, out variable) ?? false);
+    }
+    public bool TryDeclareVariable(VariableSymbol variable) => TryDeclare(variable, ref variables);
+    public ImmutableArray<VariableSymbol> GetDeclaredVariables() => variables?.Values.ToImmutableArray() ?? ImmutableArray<VariableSymbol>.Empty;
+
+    public bool TryLookupFunction(string name, out FunctionSymbol function)
+    {
+        function= null!;
+        return (functions?.TryGetValue(name, out function!) ?? false) || (Parent?.TryLookupFunction(name, out function) ?? false);
+    }
+
+    public bool TryDeclareFunction(FunctionSymbol function) => TryDeclare(function, ref functions);
+    public ImmutableArray<FunctionSymbol> GetDeclaredFunctions() => functions?.Values.ToImmutableArray() ?? ImmutableArray<FunctionSymbol>.Empty;
+    
+    static bool TryDeclare<T>(T symbol, ref Dictionary<string, T>? store) where T : Symbol
+    {
+        store ??= new();
         if (store.ContainsKey(symbol.Name)) return false;
         store.Add(symbol.Name, symbol);
         return true;
