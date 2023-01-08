119436e83f00fa8c1f19da554b2329c34e04cbf0
Rene Vogt <rene.vogt@gmx.de>
2023-01-08 14:54:16 +0100

    lazily store builtinfunctions-array

--- src/Balu/Symbols/BuiltInFunctions.cs
+++ src/Balu/Symbols/BuiltInFunctions.cs
@@ -16,8 +16,17 @@ static class BuiltInFunctions
     public static FunctionSymbol Random { get; } =
         new FunctionSymbol("random", ImmutableArray.Create(new ParameterSymbol("maximum", TypeSymbol.Integer, 0)), TypeSymbol.Integer);
 
-    public static IEnumerable<FunctionSymbol> GetBuiltInFunctions() => typeof(BuiltInFunctions)
-                                                                       .GetProperties(BindingFlags.Public | BindingFlags.Static)
-                                                                       .Where(property => property.PropertyType == typeof(FunctionSymbol))
-                                                                       .Select(property => (FunctionSymbol)property.GetValue(0)!);
+    static ImmutableArray<FunctionSymbol>? builtInFunctions;
+    public static IEnumerable<FunctionSymbol> GetBuiltInFunctions()
+    {
+        if (builtInFunctions.HasValue) return builtInFunctions.Value;
+
+        var builder = ImmutableArray.CreateBuilder<FunctionSymbol>();
+        builder.AddRange(typeof(BuiltInFunctions)
+                         .GetProperties(BindingFlags.Public | BindingFlags.Static)
+                         .Where(property => property.PropertyType == typeof(FunctionSymbol))
+                         .Select(property => (FunctionSymbol)property.GetValue(0)!));
+        builtInFunctions = builder.ToImmutable();
+        return builder;
+    }
 }
