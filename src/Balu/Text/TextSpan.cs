c30ebd5fdfa0523c5f08317882bb678bf8ab770e
Rene Vogt <rene.vogt@gmx.de>
2023-01-10 11:09:51 +0100

    add OverlapsWith method to determine overlapping text spans

--- src/Balu/Text/TextSpan.cs
+++ src/Balu/Text/TextSpan.cs
@@ -4,4 +4,5 @@ public readonly record struct TextSpan(int Start, int Length)
 {
     public int End => Start + Length;
     public override string ToString() => $"({Start}, {Length})";
+    public bool OverlapsWith(TextSpan other) => !(End < other.Start || Start > other.End);
 }
