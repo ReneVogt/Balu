c15aea50c4d168fa3acbd8789740ed4b1306fb5a
Rene Vogt <rene.vogt@gmx.de>
2023-01-12 17:59:05 +0100

    #9 introduce source generator

---
+++ src/Balu.SourceGenerator/SyntaxTreeVisitorGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Balu.SourceGenerator;
+
+[Generator]
+public sealed class SyntaxTreeVisitorGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+    public void Execute(GeneratorExecutionContext context)
+    {
+        context.AddSource(
+            "SyntaxTreeVisitor.g.cs",
+            "/* SyntaxTreeVisitor to come */");
+    }
+}
