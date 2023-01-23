34c26b59c0be6702c4f9a7fa89af85dc4af2c434
Rene Vogt <rene.vogt@gmx.de>
2023-01-23 16:25:13 +0100

    introduced post- and prefix operators, added (failing) unit tests #39

---
+++ src/Balu.Tests/ExecutionTests/PrefixExpressionTests.cs
@@ -0,0 +1,20 @@
+﻿using Balu.Tests.TestHelper;
+using Xunit;
+
+namespace Balu.Tests.CompilationTests.ExecutionTests;
+
+public partial class ExecutionTests
+{
+    [Fact]
+    public void Script_PrefixExpression_ReportsUndefinedName() => " ++[abc]".AssertScriptEvaluation("Undefined variable 'abc'.");
+    [Theory]
+    [InlineData("{ let abc = 12 ++[abc] }", "Variable 'abc' is readonly and cannot be assigned to.")]
+    [InlineData("{ var abc = true --abc", "Prefix operator '--' cannot be applied to type 'bool'.")]
+    [InlineData("{ var abc = \"\" ++abc", "Prefix operator '++' cannot be applied to type 'string'.")]
+    public void Script_PrefixExpression_ReportsDiagnostic(string code, string? diagnostics) => code.AssertScriptEvaluation(diagnostics);
+
+    [Theory]
+    [InlineData("var a = 12 var b = ++a a+b", 26)]
+    [InlineData("var a = 12 --a", 11)]
+    public void Script_PrefixExpression_CorrectResult(string code, object result) => code.AssertScriptEvaluation(value: result);
+}
---
+++ src/Balu/Syntax/PostfixExpressionSyntax.cs
@@ -0,0 +1,13 @@
+﻿namespace Balu.Syntax;
+
+public sealed partial class PostfixExpressionSyntax : ExpressionSyntax
+{
+    public override SyntaxKind Kind => SyntaxKind.PrefixExpression;
+    public SyntaxToken IdentifierToken { get; set; }
+    public SyntaxToken OperatorToken { get; set; }
+    internal PostfixExpressionSyntax(SyntaxTree syntaxTree, SyntaxToken identifierToken, SyntaxToken operatorToken) : base(syntaxTree)
+    {
+        IdentifierToken = identifierToken;
+        OperatorToken = operatorToken;
+    }
+}
