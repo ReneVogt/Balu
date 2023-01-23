77c07753b8ca6f3f717b1e8a41d84c4eabf6edf6
Rene Vogt <rene.vogt@gmx.de>
2023-01-23 20:10:57 +0100

    emit pre- and postfix expressions there are still some parserproblems with prefix expression to fix! #39

--- src/Balu.Tests/ExecutionTests/PostfixExpressionTests.cs
+++ src/Balu.Tests/ExecutionTests/PostfixExpressionTests.cs
@@ -9,13 +9,15 @@ public partial class ExecutionTests
     public void Script_PostfixExpression_ReportsUndefinedName() => " [abc]++".AssertScriptEvaluation("Undefined variable 'abc'.");
     [Theory]
     [InlineData("{ let abc = 12 [abc]++ }", "Variable 'abc' is readonly and cannot be assigned to.")]
-    [InlineData("{ var abc = true [abc--]", "Postfix operator '--' cannot be applied to type 'bool'.")]
-    [InlineData("{ var abc = \"\" [abc++]", "Postfix operator '++' cannot be applied to type 'string'.")]
-    [InlineData("function test(){} [test++]", "Postfix operator '++' cannot be applied to type 'string'.")]
+    [InlineData("{ var abc = true [abc--]}", "Postfix operator '--' cannot be applied to type 'bool'.")]
+    [InlineData("{ var abc = \"\" [abc++]}", "Postfix operator '++' cannot be applied to type 'string'.")]
+    [InlineData("function test(){} [test]++", "Unexpected symbol kind 'FunctionSymbol', expected 'test' to be a variable or argument.")]
     public void Script_PostfixExpression_ReportsDiagnostic(string code, string? diagnostics) => code.AssertScriptEvaluation(diagnostics);
 
     [Theory]
     [InlineData("var a = 12 var b = a++ a+b", 25)]
     [InlineData("var a = 12 a--", 12)]
+    [InlineData("var a = 12 var b = 2 var c = a + b++ c", 14)]
+    [InlineData("var a = 12 var b = 2 var c = a + b-- c", 14)]
     public void Script_PostfixExpression_CorrectResult(string code, object result) => code.AssertScriptEvaluation(value: result);
 }
--- src/Balu.Tests/ParserTests/PostfixExpressionTests.cs
+++ src/Balu.Tests/ParserTests/PostfixExpressionTests.cs
@@ -64,4 +64,24 @@ public partial class ParserTests
         e.AssertToken(SyntaxKind.MinusMinusToken);
         e.AssertToken(SyntaxKind.EndOfFileToken);
     }
+    [Fact]
+    public void Parser_PostfixDecrementExpressionInBinary_CorrectExpressionStatementSyntax()
+    {
+        var tree = SyntaxTree.Parse("a = b-- c");
+        using var e = new SyntaxTreeAsserter(tree.Root);
+        e.AssertNode(SyntaxKind.CompilationUnit);
+        e.AssertNode(SyntaxKind.GlobalStatement);
+        e.AssertNode(SyntaxKind.ExpressionStatement);
+        e.AssertNode(SyntaxKind.AssignmentExpression);
+        e.AssertToken(SyntaxKind.IdentifierToken, "a");
+        e.AssertToken(SyntaxKind.EqualsToken);
+        e.AssertNode(SyntaxKind.PostfixExpression);
+        e.AssertToken(SyntaxKind.IdentifierToken, "b");
+        e.AssertToken(SyntaxKind.MinusMinusToken);
+        e.AssertNode(SyntaxKind.GlobalStatement);
+        e.AssertNode(SyntaxKind.ExpressionStatement);
+        e.AssertNode(SyntaxKind.NameExpression);
+        e.AssertToken(SyntaxKind.IdentifierToken, "c");
+        e.AssertToken(SyntaxKind.EndOfFileToken);
+    }
 }
