00ee146e806964e88c43c8b2332b8eb4586bdca2
René Vogt <rene.vogt@gmx.de>
2026-07-26 20:42:44 +0200

    remove nullability

--- src/Balu/Text/TextLocation.cs
+++ src/Balu/Text/TextLocation.cs
@@ -1,16 +1,15 @@
 ﻿namespace Balu.Text;
 
-public readonly record struct TextLocation(SourceText? Text, TextSpan Span)
+public readonly record struct TextLocation(SourceText Text, TextSpan Span)
 {
-    public string? FileName => Text?.FileName;
-    public int StartLine => Text?.GetLineIndex(Span.Start) ?? 0;
-    public int EndLine => Text?.GetLineIndex(Span.End) ?? 0;
-    public int StartCharacter => Span.Start - Text?.Lines[StartLine].Start ?? 0;
-    public int EndCharacter => Span.End - Text?.Lines[EndLine].Start ?? 0;
+    public string FileName => Text.FileName;
+    public int StartLine => Text.GetLineIndex(Span.Start);
+    public int EndLine => Text.GetLineIndex(Span.End);
+    public int StartCharacter => Span.Start - Text.Lines[StartLine].Start;
+    public int EndCharacter => Span.End - Text.Lines[EndLine].Start;
 
     public override string ToString()
     {
-        if (Text is null) return string.Empty; // happens for non-located diagnostics like "missing entry point"
         var line = Text.GetLineIndex(Span.Start);
         return $"{Text.FileName}({line + 1},{Span.Start - Text.Lines[line].Start + 1})";
     }
