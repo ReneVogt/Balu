682eba3f1ac6785a1dd35599afb879fa4d27773b
René Vogt <rene.vogt@gmx.de>
2026-08-13 16:06:21 +0200

    Handle bodyless control flow graph symbols

--- src/Balu.Tests/CompilationTests/CompilationTests.cs
+++ src/Balu.Tests/CompilationTests/CompilationTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using Balu.Diagnostics;
+using Balu.Symbols;
 using Balu.Syntax;
 using Xunit;
 
@@ -14,4 +17,55 @@ public sealed class CompilationTests
         Assert.True(compilation.Diagnostics.HasErrors());
         Assert.Throws<ArgumentException>("previous", () => Compilation.CreateScript(compilation, SyntaxTree.Parse("function a() { var x = y }")));
     }
+
+    [Fact]
+    public void WriteControlFlowGraph_ThrowsForNullFunction()
+    {
+        var compilation = Compilation.Create(SyntaxTree.Parse("function main() {}"));
+
+        Assert.Throws<ArgumentNullException>("function", () => compilation.WriteControlFlowGraph(TextWriter.Null, null!));
+    }
+
+    [Fact]
+    public void WriteControlFlowGraph_ThrowsForNullWriter()
+    {
+        var compilation = Compilation.Create(SyntaxTree.Parse("function main() {}"));
+        var function = GetFunction(compilation, "main");
+
+        Assert.Throws<ArgumentNullException>("writer", () => compilation.WriteControlFlowGraph(null!, function));
+    }
+
+    [Fact]
+    public void WriteControlFlowGraph_ThrowsForBuiltInFunction()
+    {
+        var compilation = Compilation.Create(SyntaxTree.Parse("function main() {}"));
+        var function = GetFunction(compilation, "print");
+
+        Assert.Throws<ArgumentException>("function", () => compilation.WriteControlFlowGraph(TextWriter.Null, function));
+    }
+
+    [Fact]
+    public void WriteControlFlowGraph_ThrowsForFunctionFromAnotherCompilation()
+    {
+        var compilation = Compilation.Create(SyntaxTree.Parse("function main() {}"));
+        var otherCompilation = Compilation.Create(SyntaxTree.Parse("function other() {}"));
+        var function = GetFunction(otherCompilation, "other");
+
+        Assert.Throws<ArgumentException>("function", () => compilation.WriteControlFlowGraph(TextWriter.Null, function));
+    }
+
+    [Fact]
+    public void WriteControlFlowGraph_WritesGraphForUserFunction()
+    {
+        var compilation = Compilation.Create(SyntaxTree.Parse("function main() {}"));
+        var function = GetFunction(compilation, "main");
+        using var writer = new StringWriter();
+
+        compilation.WriteControlFlowGraph(writer, function);
+
+        Assert.StartsWith("digraph G {", writer.ToString());
+    }
+
+    static FunctionSymbol GetFunction(Compilation compilation, string name) =>
+        compilation.VisibleSymbols.OfType<FunctionSymbol>().Single(function => function.Name == name);
 }
